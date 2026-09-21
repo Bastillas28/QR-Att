3a63cc5db2066ac59a4115f4332288629c78df76
Bastillas28 <Gorgonia1962@gmail.com>
2026-09-21 20:32:18 +0800

    Qr-Att_Midterm Project

---
+++ constants/colors.ts
@@ -0,0 +1,13 @@
+export const COLORS = {
+  primary: '#06347d',
+  background: '#3b82f4',
+  card: '#FFFFFF',
+  textPrimary: '#0D1B2A',
+  textSecondary: '#1545f3',
+  textOnPrimary: '#262cd9',
+  surface: '#262cd9',
+  border: '#262cd9',
+  shadow: '#0D47A1',
+  danger: '#DC3545',
+  success: '#28A745',
+} as const;
